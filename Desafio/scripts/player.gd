3921048082730d79532d8b207231a033c828b857
NahuelIglesias <nahueliglesias1999@gmail.com>
2022-08-30 00:16:42 -0300

    Added x-axis movement functions, cosmetic changes pending

---
+++ Desafio/scripts/player.gd
@@ -0,0 +1,18 @@
+extends Sprite
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = Vector2(375,350)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var movePosition = Vector2(0,0)
+	
+	if (Input.is_action_pressed("mover_izq")):
+		movePosition.x += -10
+	if (Input.is_action_pressed("mover_der")):
+		movePosition.x += 10
+	
+	position += movePosition
