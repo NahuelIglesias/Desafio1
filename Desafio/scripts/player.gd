7a774cb049bcea2b18c693b702c05f6a0eb16625
NahuelIglesias <nahueliglesias1999@gmail.com>
2022-08-30 00:31:54 -0300

    Added background configs

--- Desafio/scripts/player.gd
+++ Desafio/scripts/player.gd
@@ -3,7 +3,7 @@ extends Sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(375,350)
+	position = Vector2(375,305)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
